f64c2dd1723f3c36266522c51b110d6a3211e041
Benjamin Strogg312 Dessailly <strogg312@gmail.com>
2015-08-02 17:46:37 +0200

    Modif de l'accès à la BDD

--- scripts_bdd/bdd.php
+++ scripts_bdd/bdd.php
@@ -1,10 +1,13 @@
 <?php
-	try
+	function connectNow($bdd)
 	{
-		$bdd = new PDO('mysql:host=localhost;dbname=skynet;charset=utf-8', 'root', ''); //L'accès sera plus sécurisé par la suite, les tests se faisant sur serveur virtuel en Intranet
-	}
-	catch(Exception $e)
-	{
-		die ('Erreur : ' . $e->getMesage());
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=skynet;charset=utf-8', 'root', ''); //L'accès sera plus sécurisé par la suite, les tests se faisant sur serveur virtuel en Intranet
+		}
+		catch(Exception $e)
+		{
+			die ('Erreur : ' . $e->getMesage());
+		}
 	}
 ?>
