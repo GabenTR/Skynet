6bfc1677041a5e160d249f6aa75da7b983611b1b
McClu <clu.xyz@gmail.com>
2015-08-02 17:20:16 +0200

    Modifications: index + style + login.php + content/images

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Skynet</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+  <body>
+    <?php include("pages/login.php"); ?>
+  </body>
+</html>
