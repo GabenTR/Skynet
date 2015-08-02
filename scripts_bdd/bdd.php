0ca69b11c490f23c8a01b26a3e6715f37a786497
Benjamin Strogg312 Dessailly <strogg312@gmail.com>
2015-08-02 18:52:40 +0200

    Apport de corrections mineures

--- scripts_bdd/bdd.php
+++ scripts_bdd/bdd.php
@@ -3,11 +3,11 @@
 	{
 		try
 		{
-			$bdd = new PDO('mysql:host=localhost;dbname=skynet;charset=utf-8', 'root', ''); //L'accès sera plus sécurisé par la suite, les tests se faisant sur serveur virtuel en Intranet
+			$bdd = new PDO('mysql:host=localhost;dbname=skynet;charset=utf8', 'root', ''); //L'accès sera plus sécurisé par la suite, les tests se faisant sur serveur virtuel en Intranet
 		}
 		catch(Exception $e)
 		{
-			die ('Erreur : ' . $e->getMesage());
+			die ('Erreur : ' . $e->getMessage());
 		}
 	}
 ?>
--- scripts_bdd/messages.php
+++ scripts_bdd/messages.php
@@ -1,17 +1,23 @@
 <?php
 	function createTableMessage($bdd, $conversation)
 	{
-		$sql = "CREATE TABLE Message? (idMessage int PRIMARY KEY NOT NULL, contenu text NOT NULL, pseudo int NOT NULL, datePost date NOT NULL, heurePost time NOT NULL, conversation int NOT NULL)";
-		$bdd->prepare($sql);
-		$reponse = $bdd->execute(array($conversation));
+		$sql = "CREATE TABLE Message". $conversation ." (idMessage int PRIMARY KEY NOT NULL, contenu text NOT NULL, pseudo int NOT NULL, datePost date NOT NULL, heurePost time NOT NULL, conversation int NOT NULL)";
+		//$reponse = $bdd->prepare($sql);
+		$reponse = $bdd->query($sql);
+		
+		$reponse = $bdd->prepare("ALTER TABLE Message". $conversation ." ADD INDEX(conversation)");
+		$reponse->execute(array());
+		
+		$reponse = $bdd->prepare("ALTER TABLE Message". $conversation ." ADD INDEX(pseudo)");
+		$reponse->execute(array());
 
-		$sql = "ALTER TABLE Message? ADD CONSTRAINT messageConv FOREIGN KEY conversation REFERENCES Conversation(idConversation)";
-		$bdd->prepare($sql);
-		$reponse = $bdd->execute(array($conversation));
+		$sql = "ALTER TABLE Message". $conversation ." ADD CONSTRAINT messageConv FOREIGN KEY (conversation) REFERENCES Conversation(idConversation)";
+		$reponse = $bdd->prepare($sql);
+		$reponse->execute(array());
 
-		$sql = "ALTER TABLE Message? ADD CONSTRAINT messageConv FOREIGN KEY pseudo REFERENCES Pseudo(idPseudo)";
-		$bdd->prepare($sql);
-		$reponse = $bdd->execute(array($conversation));
+		$sql = "ALTER TABLE Message". $conversation ." ADD CONSTRAINT messageOwner FOREIGN KEY (pseudo) REFERENCES Pseudo(idPseudo)";
+		$reponse = $bdd->prepare($sql);
+		$reponse->execute(array());
 	}
 
 	function obtainMessages($bdd, $conversation)
@@ -25,7 +31,7 @@
 
 	function addMessage($bdd, $contenu, $pseudo, $date, $heure, $conversation)
 	{
-		$sql = "INSERT INTO Message". $conversation ." (contenu,pseudo,datePost,heurePost,conversation) VALUES (?,(SELECT idPseudo FROM Pseudo WHERE nomPseudo LIKE ?),?,?,(SELECT idConversation FROM Conversation WHERE titre LIKE ?))");
+		$sql = "INSERT INTO Message". $conversation ." (contenu,pseudo,datePost,heurePost,conversation) VALUES (?,(SELECT idPseudo FROM Pseudo WHERE nomPseudo LIKE ?),?,?,(SELECT idConversation FROM Conversation WHERE titre LIKE ?))";
 		$bdd->prepare($sql);
 		$reponse = $bdd->execute(array($contenu,$pseudo,$date,$heure,$conversation));
 	}
