a28ee1a639e7abbf65e4b68c29e6d80817524f4b
Benjamin Strogg312 Dessailly <strogg312@gmail.com>
2015-08-02 16:51:06 +0200

    Ajout de la connexion à la base de données

--- scripts_bdd/bdd.php
+++ scripts_bdd/bdd.php
@@ -1,3 +1,10 @@
 <?php
-
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=skynet;charset=utf-8', 'root', ''); //L'accès sera plus sécurisé par la suite, les tests se faisant sur serveur virtuel en Intranet
+	}
+	catch(Exception $e)
+	{
+		die ('Erreur : ' . $e->getMesage());
+	}
 ?>
